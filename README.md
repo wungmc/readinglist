9398dd8d588db9bed15f36f61bf05a07136b77df
王明初 <wangmingchu@wdai.com>
2018-09-27 21:55:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+spring boot 1 example
+
+- spring boot 1.5.16
+- java 8
---
+++ src/main/java/com/wung/ReadingListApplication.java
@@ -0,0 +1,12 @@
+package com.wung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListApplication.class, args);
+	}
+}
